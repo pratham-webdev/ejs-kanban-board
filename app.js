b8cbaf7844a2ea69ca45c7bcf254ecb5936e3988
pratham-webdev <pratham.webdev@gmail.com>
2021-05-24 00:36:10 +0530

    added mongo and mongoose

--- app.js
+++ app.js
@@ -1,45 +1,83 @@
 const express = require('express');
 
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/trello", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+
 const app = express();
 
 app.set("view engine","ejs");
 
 app.use(express.urlencoded({extended: true}));
 
-app.use(express.static("public"));
+app.use(express.static(__dirname+"/public/"));
 
 const kanbanBoards = [];
 
 const items = [];
 
-app.get('/', (req,res) => {
-    res.render('index', {boards:kanbanBoards});
+const listItemSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    }
 });
 
-app.post('/', (req,res) => {
-    if(req.body.button === "list"){
-        const listName = {
-            id: kanbanBoards.length,
-            name : req.body.list,
-            listItems : []
-        };
-        kanbanBoards.push(listName);
-    }
-    else{
-        kanbanBoards.forEach((board) => {
-            let listID = Number(req.body.button);
-            if( listID === board.id){
-                const listItem = {
-                    id: board.listItems.length,
-                    detail : req.body.listItem,
-                    author : "Yuji",
-                    image : "img/yuji.png"
-                }
-                kanbanBoards[board.id].listItems.push(listItem);
-            }
-        });
-    }
+const ListSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    listItems : [listItemSchema]
+});
+
+const Kanban = mongoose.model('Kanban', ListSchema);
+const ListItem = mongoose.model('List',listItemSchema);
+
+app.get('/', (req,res) => {
+    Kanban.find({}, (err, lists) => {
+    res.render('index', {boards:lists});
+    })
+});
+
+app.post('/add-list', (req,res) => {
+
+    const newListName = req.body.listName;
+    const newList = new Kanban({name:newListName});
+    newList.save();
+    res.redirect("/")
+});
+
+app.post('/add-list-item/:listID', (req,res) => {
+
+    const newListItem = new ListItem({
+        name: req.body.listItemName 
+    });
+    const listID = req.params.listID;
+    Kanban.findOne({ _id : listID }, (err, foundList) => {
+        foundList.listItems.push(newListItem);
+        foundList.save();
+        res.redirect("/");
+    });
+});
+
+app.post('/delete-list/:id', (req,res) => {
+    Kanban.deleteOne({_id:req.params.id}, (err) => {
+        if(err){
+            console.log(err);
+        }
+    });
     res.redirect('/');
+});
+
+app.post('/delete-list-item/:listid/:itemid', (req,res) => {
+
+    Kanban.findOneAndUpdate({ _id:req.params.listid}, {$pull : {listItems: {_id:req.params.itemid}}}, (err) => {
+        if(err){
+            console.log(err);
+        }
+    });
+    res.redirect("/");
 });
 
 app.listen(3000, () => {
